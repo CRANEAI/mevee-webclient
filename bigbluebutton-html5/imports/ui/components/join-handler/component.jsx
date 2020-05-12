c4988ceac4de4790ce2c868de66fe143274306bf
Ryan Hickman <kingsrborn@gmail.com>
2020-05-12 12:14:25 -0400

    API pointer

--- bigbluebutton-html5/imports/ui/components/join-handler/component.jsx
+++ bigbluebutton-html5/imports/ui/components/join-handler/component.jsx
@@ -157,7 +157,7 @@ class JoinHandler extends Component {
     };
 
     // use enter api to get params for the client
-    const url = `https://www.meveepro.com/bigbluebutton/api/enter?sessionToken=${sessionToken}`;
+    const url = `https://live.meveepro.com/bigbluebutton/api/enter?sessionToken=${sessionToken}`;
     const fetchContent = await fetch(url, { credentials: 'same-origin' });
     const parseToJson = await fetchContent.json();
     const { response } = parseToJson;
