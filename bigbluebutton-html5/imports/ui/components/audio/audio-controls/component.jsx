da135b25008614ccf3dfe399b799a38400b2365c
Ryan Hickman <kingsrborn@gmail.com>
2020-05-13 00:19:04 -0400

    map om

--- bigbluebutton-html5/imports/ui/components/audio/audio-controls/component.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/audio-controls/component.jsx
@@ -58,7 +58,7 @@ class AudioControls extends PureComponent {
 
       window.callm = () =>{
         alert("triggered")
-        this.listenBtn.current.click();
+        this.listenBtn.click();
       }
 
     }
