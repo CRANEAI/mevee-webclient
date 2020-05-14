4b8bdd74b94f7495b3bc2f01c94264bf277dbb48
Ryan Hickman <kingsrborn@gmail.com>
2020-05-13 22:24:30 -0400

    ff

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-item/user-dropdown/component.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-item/user-dropdown/component.jsx
@@ -191,11 +191,9 @@ class UserDropdown extends PureComponent {
 
       case 'toggleVoice':
       console.log('setting viewer')
-      this.toggleVoice(e.userId)
+      this.props.toggleVoice(e.userId)
       let mid = this.props.requestUserInformation(e.userId)
       console.log(mid)
-     
-
       break;
 
 
